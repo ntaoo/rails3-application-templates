ae16801d8083d9638f21c2175ece35d8db079cc6
naotoueo <naotoueo@gmail.com>
2011-06-08 23:35:22 +0900

    first commit with one template

---
+++ rails3-devise-rspec-cucumber_template.rb
@@ -0,0 +1,32 @@
+# simple template for rails3 and rails3.1 pre
+# set up devise, test tools, and create initial commit
+
+# authentication and authorization
+gem "devise"
+
+# test tools
+gem "rspec-rails", :group => [ :development, :test ]
+gem "factory_girl_rails", :group => [ :development, :test ]
+gem "cucumber", :group => [ :development, :test ]
+gem "autotest", :group => :test
+
+run 'bundle install'
+
+# rspec setup
+generate 'rspec:install'
+run "echo '--format documentation' >> .rspec"
+
+# authentication and authorization setup
+generate "devise:install"
+
+remove_file 'public/index.html'
+
+# commit to git
+git :init
+git :add => "."
+git :commit => "-a -m 'create initial commit'"
+
+say <<-eos
+  ============================================================================  
+  Don't forget to scroll up for important messages from installed generators.
+eos
